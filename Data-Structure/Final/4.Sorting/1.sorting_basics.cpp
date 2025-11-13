8af699c92de30246f527c9491095a2c048c3560c
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-11-13 20:28:36 +0600

    Create 1.sorting_basics.cpp

---
+++ Data-Structure/Final/4.Sorting/1.sorting_basics.cpp
@@ -0,0 +1,20 @@
+// Sorting 
+
+// Sorting holo akta process ja diye amra specific order e data arrange korte parbo
+// jmn ascending order (small to large) or descending order(large to small)
+
+
+
+// Types of Sorting
+
+// Iterative Sorting algorithm (comparisos based)
+// 1) selection sort
+// 2) bubble sort
+// 3) insertion sort
+// etar time complexity O(n^2)
+
+// Recursive Sorting algorithm (comparisos based)
+// 1) Marge Sort
+// 2) Quick Sort
+// etar time complexity O(n log n)
+// eta beshi efficient
