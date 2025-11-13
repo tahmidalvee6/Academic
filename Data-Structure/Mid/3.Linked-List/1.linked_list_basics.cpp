e3067fea486a1bfa05bb1cfdd6dc6f3fc65df08c
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-11-13 20:27:04 +0600

    Create 1.linked_list_basics.cpp

---
+++ Data-Structure/Mid/3.Linked-List/1.linked_list_basics.cpp
@@ -0,0 +1,53 @@
+// Basics
+
+// What is Linked List ?
+// Linked List is a Lenear Data Structure used to store a list of values
+
+
+// What is aray ?
+// Array is a single memory block with  pertition
+// If a array is a size of 5 size then block of memory will be 5 partition
+// array te single memory block thake jetate onk gula partition theke
+
+
+
+// R linked lsit e multiple memory block thake jeta akta arektar shthe linked hoy
+
+
+
+// Challenge of array (array te j pblm thakar jnno linked list dorkar) 
+// 1. static size / fixed size :
+// jkhn amra akta array define kori amdr size bole dite hoy...
+// pore size ta change ta change korte parbo na...karon oi size er array memory te allocate hoy
+// amra jodi 200 size er akta array banai and  akhn amdr 250 jon ase...tw amra ei array ta baraite parbo na...notun array banaite hobe
+// jodi 180 jon lage...taile amdr memory te baki jaega gula wasted jabe...array size komano jae na
+
+//2. Contguous memory allocation :
+// amdr memory te 100 byte er jaega khli ase...amdr arrayr jnno lagbe o 100 byte...kintu memory te jaega gula contiguaous mane akshte hoite hobe
+// mne jodi 20 byte er akta block khli ase abr memoryr arek side e abr 80 byte khli ase...
+// tao amra arrayr jnno declare korte parbo na karon akshthe nai tai
+
+//3. Inserting and deleting is costly in array :
+// jodi amdr akta array te kon kisu add korte hoy amra ta parbo na...
+// er jnno alada akta array nite hobe jetate oi notun array ta rakhte pari...delete er khetre o same
+// tai array use korle memory te beshi storage lage
+
+
+// Advantages of a linked list over an array :
+//1. Dynamic size allocate hoy :
+// mane amdr size ta specify kore bole dite hoy na
+
+//2. Non contiguous memory allocation :
+// mane element shb gula pashapshi thakte hobe eirokom na
+// akta 100 te thakle arekta 101 te e thakte hobe amn kono kthe nai...chaile 110 o thakte pare
+
+
+// 3.Insertion and deletion is  non expensive :
+// jdi akta element add korte hoy taile 2 element er majhe oi element ta link kore dibo
+// jdi majher akta element delete korte hoy taile pore element tar sthe ager element ta link kore dibo
+// alada vabe memory te jaega neya lagtese na
+
+
+
+
+
