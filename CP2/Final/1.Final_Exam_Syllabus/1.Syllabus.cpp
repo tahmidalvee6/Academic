29b1130939e76e2485ea760f09eb676c851118b9
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-01-16 16:17:16 +0600

    Update and rename CP2/Final/1.final to CP2/Final/1.Final_Exam_Syllabus/1.Syllabus.cpp

---
+++ CP2/Final/1.Final_Exam_Syllabus/1.Syllabus.cpp
@@ -0,0 +1,14 @@
+1. Number Theory [7 marks] 
+ - Primality check and generation
+ - Prime Factorization
+ - Sieve of Eratosthenes
+ - Bigmod
+ 
+2. Binary Search [8 marks] 
+ - Binary search, lower bound, upper bound
+
+3. Sorting [8 marks]
+ - Merge Sort, Quick Sort, STL comparator based sort, basic sorting related problems
+
+4. Graph  [7 marks] 
+- Adjacent Matrix/ Adjacent List, BFS, DFS
