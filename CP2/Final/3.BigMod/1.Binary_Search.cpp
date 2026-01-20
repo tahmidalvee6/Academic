57244ab331e66bea16bf8c94a519877df13e08ea
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-01-21 05:31:58 +0600

    Update 1.Binary_Search.cpp

--- CP2/Final/3.BigMod/1.Binary_Search.cpp
+++ CP2/Final/3.BigMod/1.Binary_Search.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int n, int target) {
-    int left = 0, right = n - 1;
-
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-
-        if (arr[mid] == target)
-            return mid; // Found
-        else if (arr[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return -1; // Not found
-}
-
 int main() {
     int n, target;
     cin >> n;
     int arr[n];
-    for(int i=0; i<n; i++)
+    for(int i = 0; i < n; i++)
         cin >> arr[i];
     cin >> target;
 
-    int index = binarySearch(arr, n, target);
+    int left = 0, right = n - 1;
+    int index = -1;
+
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(arr[mid] == target) {
+            index = mid;
+            break;
+        } else if(arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
     if(index != -1)
         cout << "Found at index " << index;
     else
@@ -33,6 +31,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
