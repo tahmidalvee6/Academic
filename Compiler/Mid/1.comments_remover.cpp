d887c08ff45746fcee9c8b797903f244c9ecac77
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-05-03 07:30:30 +0600

    Update 1.comments_remover.cpp

--- Compiler/Mid/1.comments_remover.cpp
+++ Compiler/Mid/1.comments_remover.cpp
@@ -1,79 +1,85 @@
-#include <iostream> 
-#include <fstream> 
-using namespace std; 
-int main() { 
-    ifstream fin("input.c"); 
-    ofstream fout("output.c"); 
- 
-    if (!fin.is_open() || !fout.is_open()) { 
-        cout << "Error opening file!" << endl; 
-        return 1; 
-    } 
- 
-    char c; 
-    enum State { NORMAL, IN_STRING, IN_CHAR, IN_SL_COMMENT, IN_ML_COMMENT }; 
-    State state = NORMAL; 
- 
-    while (fin.get(c)) { 
-        if (state == NORMAL) { 
-            if (c == '/') { 
-                if (fin.peek() == '/') { 
-                    state = IN_SL_COMMENT; 
-                    fin.get(c); // consume second '/' 
-                    continue; 
-                } 
-                else if (fin.peek() == '*') { 
-                    state = IN_ML_COMMENT; 
-                    fin.get(c); // consume '*' 
-                    continue; 
-                } 
-                else { 
-                    fout.put(c); 
-                } 
-            } 
-            else if (c == '"') { 
-                state = IN_STRING; 
-                fout.put(c); 
-            } 
-            else if (c == '\'') { 
-                state = IN_CHAR; 
-                fout.put(c); 
-            } 
-            else { 
-                fout.put(c); 
-            } 
-        } 
-        else if (state == IN_STRING) { 
-            fout.put(c); 
-            if (c == '\\') { // escape sequence 
-                if (fin.get(c)) fout.put(c); 
-            } else if (c == '"') { 
-                state = NORMAL; 
-            } 
-        } 
-        else if (state == IN_CHAR) { 
-            fout.put(c); 
-            if (c == '\\') { // escape sequence 
-                if (fin.get(c)) fout.put(c); 
-            } else if (c == '\'') { 
-                state = NORMAL; 
-            } 
-        } 
-        else if (state == IN_SL_COMMENT) { 
-            if (c == '\n') { 
-                fout.put('\n'); 
-                state = NORMAL; 
-            } 
-        } 
-        else if (state == IN_ML_COMMENT) { 
-            if (c == '*' && fin.peek() == '/') { 
-                fin.get(c); // consume '/' 
-                state = NORMAL; 
-            } 
-} 
-} 
-fin.close(); 
-fout.close(); 
-cout << "Comments removed successfully! Check output.c" << endl; 
-return 0; 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string code = "";
+    string line;
+
+    cout << "Enter your code:" << endl;
+
+    while (getline(cin, line)) {
+        if (line == "END") break;
+        code += line + "\n";
+    }
+
+    string state = "NORMAL";
+
+    for (int i = 0; i < code.size(); i++) {
+        char c = code[i];
+        char next = '\0';
+        if (i + 1 < code.size()) {
+            next = code[i + 1];
+        }
+
+        if (state == "NORMAL") {
+            if (c == '/' && next == '/') {
+                state = "SINGLE";
+                i++;
+            }
+            else if (c == '/' && next == '*') {
+                state = "MULTI";
+                i++;
+            }
+            else if (c == '"') {
+                state = "STRING";
+                cout << c;
+            }
+            else if (c == '\'') {
+                state = "CHAR";
+                cout << c;
+            }
+            else {
+                cout << c;
+            }
+        }
+
+        else if (state == "SINGLE") {
+            if (c == '\n') {
+                cout << '\n';
+                state = "NORMAL";
+            }
+        }
+
+        else if (state == "MULTI") {
+            if (c == '*' && next == '/') {
+                state = "NORMAL";
+                i++;
+            }
+        }
+
+        else if (state == "STRING") {
+            cout << c;
+            if (c == '\\') {
+                cout << next;
+                i++;
+            }
+            else if (c == '"') {
+                state = "NORMAL";
+            }
+        }
+
+        else if (state == "CHAR") {
+            cout << c;
+            if (c == '\\') {
+                cout << next;
+                i++;
+            }
+            else if (c == '\'') {
+                state = "NORMAL";
+            }
+        }
+    }
+
+    return 0;
 }
