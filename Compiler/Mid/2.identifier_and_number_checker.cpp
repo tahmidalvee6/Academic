4bf4c486b283fa860f4e926960581871f5a3e5eb
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2026-05-03 06:29:42 +0600

    Refactor identifier and number checker functions

--- Compiler/Mid/2.identifier_and_number_checker.cpp
+++ Compiler/Mid/2.identifier_and_number_checker.cpp
@@ -1,27 +1,60 @@
-#include <iostream> 
-#include <cctype> 
-#include <string> 
-using namespace std; 
-bool isIdentifier(const string& s) { 
-if (!(isalpha(s[0]) || s[0] == '_')) return false; 
-for (int i = 1; i < s.size(); i++) { 
-if (!(isalnum(s[i]) || s[i] == '_')) return false; 
-    } 
-    return true; 
-} 
- 
-bool isNumber(const string& s) { 
-    bool dotSeen = false, expSeen = false; 
-    int i = 0; 
-    if (s[i] == '+' || s[i] == '-') i++; 
-    for (; i < s.size(); i++) { 
-        if (isdigit(s[i])) continue; 
-        else if (s[i] == '.' && !dotSeen) dotSeen = true; 
-        else if ((s[i] == 'e' || s[i] == 'E') && !expSeen) { 
-            expSeen = true; 
-            if (i+1 < s.size() && (s[i+1]=='+' || s[i+1]=='-')) i++; 
-        } 
-        else return false; 
-    } 
-    return true; 
-} 
+#include <bits/stdc++.h>
+using namespace std;
+
+int isIdentifier(string s) {
+    if (!isalpha(s[0]) && s[0] != '_') {
+        return 0;
+    }
+    for (int i = 1; i < s.size(); i++) {
+        char c = s[i];
+        if (!isalpha(c) && !isdigit(c) && c != '_') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int isNumber(string s) {
+    int dot = 0;
+    int exp = 0;
+    int start = 0;
+    if (s[0] == '+' || s[0] == '-') {
+        start = 1;
+    }
+    for (int i = start; i < s.size(); i++) {
+        char c = s[i];
+        if (isdigit(c)) {
+            continue;
+        }
+        else if (c == '.' && dot == 0) {
+            dot = 1;
+        }
+        else if ((c == 'e' || c == 'E') && exp == 0) {
+            exp = 1;
+            char next = s[i+1];
+            if (next == '+' || next == '-') {
+                i++;
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    string token;
+    cout << "Enter a token: ";
+    cin >> token;
+    if (isIdentifier(token)) {
+        cout << token << " is a valid Identifier" << endl;
+    }
+    else if (isNumber(token)) {
+        cout << token << " is a valid Number" << endl;
+    }
+    else {
+        cout << token << " is Invalid" << endl;
+    }
+    return 0;
+}
